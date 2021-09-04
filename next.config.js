178627e2e3dc859d981308ccc222e9a4473b205a
Picasso Punk <picasso@instapunks.com>
2021-09-04 09:01:25 +0300

    remove prefix

--- next.config.js
+++ next.config.js
@@ -6,21 +6,10 @@
   https://nextjs.org/docs/api-reference/next.config.js/environment-variables
 */
 
-const package = require('./package.json');
-
-const isProduction = 'production' === `${process.env.NODE_ENV}`;
-
-const basePath = isProduction ? `/${package.name}` : '';
-
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = {
-  target: "serverless",
   trailingSlash: true,
-  reactStrictMode: true,
-  basePath,
-  env: {
-    ASSETS_PREFIX: basePath,
-  },
+  reactStrictMode: true
 };
