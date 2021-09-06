73508a4f97f24ce299dbd389083e558c57b3de7d
Picasso Punk <picasso@instapunks.com>
2021-09-06 06:47:27 +0300

    star slider

--- src/components/StarsSlider/StarsSlider.tsx
+++ src/components/StarsSlider/StarsSlider.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 
 import styles from './StarsSlider.module.scss';
 import Star from 'components/Star/Star';
+import Carousel, {
+  autoplayPlugin,
+  slidesToShowPlugin,
+} from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 type StarsSliderProps = unknown;
 
@@ -21,16 +26,97 @@ const stars = [
 export const StarsSlider: React.FC<StarsSliderProps> = ({ ...props }) => {
   return (
     <div className={styles.starsSlider} {...props}>
-      {stars.map(({ name, insta }, index) => (
-        <Star
-          key={insta}
-          img={`./star-${('0' + (index + 1)).substr(-2)}.png`}
-          name={name}
-          nickname={insta}
-          verified={true}
-          className={styles.star}
-        />
-      ))}
+      <Carousel
+        plugins={[
+          'infinite',
+          {
+            resolve: autoplayPlugin,
+            options: {
+              interval: 1500,
+            },
+          },
+          {
+            resolve: slidesToShowPlugin,
+            options: {
+              numberOfSlides: 6,
+            },
+          },
+        ]}
+        breakpoints={{
+          340: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 1,
+                },
+              },
+            ],
+          },
+          480: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 2,
+                },
+              },
+            ],
+          },
+          800: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 3,
+                },
+              },
+            ],
+          },
+          1000: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 4,
+                },
+              },
+            ],
+          },
+          1200: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 5,
+                },
+              },
+            ],
+          },
+          1400: {
+            plugins: [
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 6,
+                },
+              },
+            ],
+          },
+        }}
+        animationSpeed={500}
+      >
+        {stars.map(({ name, insta }, index) => (
+          <Star
+            key={insta}
+            img={`./star-${('0' + (index + 1)).substr(-2)}.png`}
+            name={name}
+            nickname={insta}
+            verified={true}
+            className={styles.star}
+          />
+        ))}
+      </Carousel>
     </div>
   );
 };
